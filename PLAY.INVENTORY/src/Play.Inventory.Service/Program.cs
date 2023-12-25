38b930c087539f8911bfe8339db3424cd14431c0
Vaibhav <vaibhavkumar5219@gmail.com>
2023-12-25 18:11:12 +0530

    Items controller implemented in Play.Inventory

--- PLAY.INVENTORY/src/Play.Inventory.Service/Program.cs
+++ PLAY.INVENTORY/src/Play.Inventory.Service/Program.cs
@@ -18,7 +18,7 @@ namespace Play.Inventory.Service
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>++
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
--- PLAY.INVENTORY/src/Play.Inventory.Service/Startup.cs
+++ PLAY.INVENTORY/src/Play.Inventory.Service/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Play.Common.MongoDB;
+using Play.Inventory.Service.Entities;
 
 namespace Play.Inventory.Service
 {
@@ -26,7 +21,8 @@ namespace Play.Inventory.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMongo()
+                    .AddMongoRepository<InventoryItem>("inventoryitems");
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
