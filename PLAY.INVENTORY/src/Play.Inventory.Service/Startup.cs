cdf280fced20c45b46c743d03013af9626569ae4
Vaibhav <vaibhavkumar5219@gmail.com>
2023-12-30 11:52:37 +0530

    Network timeout added

--- PLAY.INVENTORY/src/Play.Inventory.Service/Startup.cs
+++ PLAY.INVENTORY/src/Play.Inventory.Service/Startup.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Net.Http;
+using Amazon.Auth.AccessControlPolicy;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +11,10 @@ using Microsoft.OpenApi.Models;
 using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Microsoft.Extensions.Http;
+using Polly;
+using Amazon.Runtime.Internal.Util;
+using Microsoft.Extensions.Logging;
 
 namespace Play.Inventory.Service
 {
@@ -26,10 +33,26 @@ namespace Play.Inventory.Service
             services.AddMongo()
                     .AddMongoRepository<InventoryItem>("inventoryitems");
 
+            Random jitterer = new Random();
+
+
             services.AddHttpClient<CatalogClient>(client =>
             {
                 client.BaseAddress = new Uri("https://localhost:5001");
-            });
+            })
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutException>().WaitAndRetryAsync(
+                5,
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                                    + TimeSpan.FromMilliseconds(jitterer.Next(1, 1000)),
+                onRetry: (outcome, timeSpan, retryAttempt) => 
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<CatalogClient>>()?
+                            .LogWarning($"Delaying for {timeSpan.TotalSeconds} seconds, then making retry {retryAttempt}");
+
+                }
+            ))
+            .AddPolicyHandler(Polly.Policy.TimeoutAsync<HttpResponseMessage>(1));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
